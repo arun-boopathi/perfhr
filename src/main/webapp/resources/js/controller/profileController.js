ce167519ea8f35c70d6fe6582eec1e01d87906fd
s.ellappan <s.ellappan@perficient.com>
2016-03-14 15:32:56 +0530

    Included user info on header

--- src/main/webapp/resources/js/controller/profileController.js
+++ src/main/webapp/resources/js/controller/profileController.js
@@ -1,9 +1,13 @@
+var user;
+
 angular.module('profile.controller', []).
 	/* Profile controller */
     controller('profileController', function($scope, profileAPIservice) {
       $scope.profile = null;
       profileAPIservice.getProfileDetails().success(function (response) {
     	  console.log('response ', response);
+    	  user = response;
+    	  $('#welcomeMsg').html('Welcome '+response.lastName+', '+response.firstName);
           $scope.profile = response; 
     });
 });
