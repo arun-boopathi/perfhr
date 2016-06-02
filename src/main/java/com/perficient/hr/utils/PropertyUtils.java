4f83369e292729eeae543134edaf71aa64717087
stedqt <ste.dqt@live.com>
2016-06-02 13:16:02 +0530

    Application Config Cleanup

--- src/main/java/com/perficient/hr/utils/PropertyUtils.java
+++ src/main/java/com/perficient/hr/utils/PropertyUtils.java
@@ -1,14 +1,16 @@
 package com.perficient.hr.utils;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@ComponentScan(basePackages = "com.perficient.hr")
-@PropertySource(value = {"classpath:appconfig.properties"})
+@PropertySources({
+    @PropertySource("classpath:appconfig.properties"),
+    @PropertySource("classpath:config.properties")
+})
 public class PropertyUtils {
 
 	/*
@@ -17,7 +19,10 @@ public class PropertyUtils {
      */
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
+    	PropertySourcesPlaceholderConfigurer configurer=new PropertySourcesPlaceholderConfigurer();
+    	configurer.setIgnoreResourceNotFound(true);
+    	configurer.setIgnoreUnresolvablePlaceholders(true);
+    	return configurer;
     }
     
 }
