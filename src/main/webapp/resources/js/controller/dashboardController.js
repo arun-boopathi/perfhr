0a6a324886100f5bc60ed40db8c2d0fad0778f4f
s.ellappan <s.ellappan@perficient.com>
2016-09-26 11:37:23 +0530

    Fixed issue with displaying version in all pages.

--- src/main/webapp/resources/js/controller/dashboardController.js
+++ src/main/webapp/resources/js/controller/dashboardController.js
@@ -1,7 +1,4 @@
 /*Dashboard Controller*/
 mainApp.controller('dashboardController', function($scope, dashboardAPIservice) {
-	dashboardAPIservice.getVersion().success(function (response) {
-        $('#version').html('Version: '+response.entity);
-    });
 }).controller('approvalReqCtrl', function() {
 });
--- src/main/webapp/resources/js/controller/headerController.js
+++ src/main/webapp/resources/js/controller/headerController.js
@@ -1,7 +1,10 @@
 /*Header Controller*/
-mainApp.controller('headerController', function($scope, user, profileAPIservice) {
+mainApp.controller('headerController', function($scope, user, profileAPIservice, dashboardAPIservice) {
     profileAPIservice.getProfileDetails().success(function (response) {
         $scope.user = response.entity;
         user.loggedUser = response.entity;
+    });
+    dashboardAPIservice.getVersion().success(function (response) {
+        $('#version').html('Version: '+response.entity);
     });
 });
